9ad7a480abeb535cd18ba7c9c1475fb73c91a2c8
Роман <romaxakoval@gmail.com>
2022-01-09 23:26:20 +0300

    refactor component Money

--- src/components/UI/Money.vue
+++ src/components/UI/Money.vue
@@ -1,10 +1,8 @@
 <template>
   <input
-    :value="formattedNumber"
     ref="input"
     class="ui-money"
     @input="onInput($event)"
-    @keypress="onKeyPress($event)"
     @change="onChange"
   />
 </template>
@@ -29,57 +27,35 @@ export default {
     };
   },
 
-  created() {
-    if (this.value) this.valueInput = this.value.toString();
-  },
-
-  computed: {
-    formattedNumber() {
-      return money.formatPrice(this.valueInput);
-    },
+  mounted() {
+    if (this.value) this.setValue(this.value.toString());
   },
 
   methods: {
     onInput(event) {
-      const value = event.target.value.replace(/[^,.\d]/g, '').replace(',', '.');
+      let value = event.target.value.replace(/[^,.\d]/g, '').replace(',', '.');
       const positionCursor = event.target.selectionStart;
       let partsNumber = value.split('.');
-      let isChangeValue = false;
-
-      if (!value) {
-        isChangeValue = true;
-      }
 
       if (!partsNumber[0] && partsNumber[1]) {
         partsNumber.splice(0, 1);
-        isChangeValue = true;
       }
 
       if (partsNumber.length > 2) {
         partsNumber = [partsNumber[0], partsNumber[1]];
-        isChangeValue = true;
       }
 
       if (partsNumber[1] && partsNumber[1].length > 2) {
         partsNumber[1] = partsNumber[1].substring(0, 2);
-        isChangeValue = true;
       }
 
-      this.valueInput = partsNumber.join('.');
+      value = partsNumber.join('.');
 
-      if (isChangeValue) {
-        this.$refs.input.value = money.formatPrice(this.valueInput);
-      }
-
+      this.setValue(value);
       this.toEmit();
 
       if (this.$refs.input.selectionStart < this.valueInput.length) {
         this.$nextTick(() => this.setCursorPosition(this.$refs.input, positionCursor));
-      }
-    },
-    onKeyPress(event) {
-      if (!/[,.0-9]/.test(event.key)) {
-        event.preventDefault();
       }
     },
     onChange() {
@@ -94,6 +70,10 @@ export default {
 
         this.toEmit();
       }
+    },
+    setValue(value) {
+      this.valueInput = value;
+      this.$refs.input.value = money.formatPrice(this.valueInput);
     },
     setCursorPosition(el, pos) {
       el.focus();
